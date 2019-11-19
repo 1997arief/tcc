f5f9600943fbc3eea93d95d9d4869cb253feb8c8
Arief M. Ikhsan <1997arief@gmail.com>
2019-11-19 20:35:24 +0700

    update minggu-09

--- minggu-08/FlaskApp/app/views.py
+++ minggu-08/FlaskApp/app/views.py
@@ -1,5 +1,10 @@
-from app import app
+from flask import render_template
+from app import app 
 
 @app.route('/')
 def home():
-   return "Hello World!"
+   return "<b>There has been a change</b>"
+
+@app.route('/template')
+def template():
+   return render_template('home.html')
